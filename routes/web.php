b4ce541389c8145802a3ee886183d7b97734f6ed
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-07-23 14:37:50 +0530

    payment collection changes in invoice

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware('guest')->group(function () {
 });
 
 // Authenticated routes
-Route::middleware('auth')->group(function () {
+  Route::middleware('auth')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
